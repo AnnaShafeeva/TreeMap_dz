005fc67aaa5bf4c8cc113ee7c64fac6b289b63c7
Anna Shafeeva <dzenborg@gmail.com>
2022-09-06 10:17:04 +0300

    Added \P{IsAlphabetic}+

--- src/PersonsLengthComparator.java
+++ src/PersonsLengthComparator.java
@@ -1,4 +1,5 @@
 import java.util.Comparator;
+import java.util.regex.Pattern;
 
 public class PersonsLengthComparator implements Comparator<Person> {
     private String[] partsSurname1;
@@ -14,12 +15,13 @@ public class PersonsLengthComparator implements Comparator<Person> {
     public int compare(Person o1, Person o2) {
         int resultCompare = Integer.compare(o1.getSurname().length(), o2.getSurname().length());
 
-        partsSurname1 = o1.getSurname().split(" ");
-        partsSurname2 = o2.getSurname().split(" ");
+        partsSurname1 = o1.getSurname().split("\\P{IsAlphabetic}+");
+        partsSurname2 = o2.getSurname().split("\\P{IsAlphabetic}+");
 
-        int resultCompareNotZero = (resultCompare == 1 || (partsSurname1.length >= quantity &&
-                partsSurname2.length >= quantity )) ? -1 : 1;
-        if (resultCompare == 0){
+        int resultCompareNotZero = (resultCompare == 1) ? -1 : 1;
+
+        if (resultCompare == 0 || (partsSurname1.length >= quantity &&
+                partsSurname2.length >= quantity )){
             if (o1.getAge() > o2.getAge()){
                 return -1;
             } else {
